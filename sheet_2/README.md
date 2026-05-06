a9b46bf17ca6dc495a6ae2ea0714072372631484
vincmarks <vimarks@students.uni-mainz.de>
2026-05-06 11:58:39 +0200

    add README.md

---
+++ sheet_2/README.md
@@ -0,0 +1,8 @@
+1. The code has been written in Julia 1.12
+
+2. To reproduce the plots open a Julia repl and execute
+    ```julia
+        include("sheet_2/sheet2.jl")
+    ```
+
+3. The plots will be saved in the `Plots` folder.
--- sheet_2/sheet2.jl
+++ sheet_2/sheet2.jl
@@ -1,10 +1,10 @@
 # 2 Bonus
 using Pkg
-Pkg.activate(".")
+Pkg.activate(@__DIR__)
 Pkg.instantiate()
 
-Pkg.add("Plots")
-Pkg.add("LaTeXStrings")
+# Pkg.add("Plots")
+# Pkg.add("LaTeXStrings")
 
 using Plots
 using LaTeXStrings
@@ -23,14 +23,14 @@ for i in 1:10
     h = 10.0^(-i)
     push!(err, res_sin(pi/4, h))
 end
-p1 = scatter(10.0 .^(-1:-1:-10) , err, yaxis=:log, xaxis=:log, label="Error", xlabel=L"10^{-i}", ylabel="Error", legend =:bottomright)
+p1 = scatter(10.0 .^(-1:-1:-10) , err, title=L"\vert f'(x_1) - (f_2-f_0)/(2h) \vert \quad vs. h ", yaxis=:log, xaxis=:log, label=L"\vert f'(x_1) - (f_2-f_0)/(2h) \vert", xlabel=L"10^{-i}", ylabel="Error", legend =:bottomright)
 vline!(p1, [sqrt(eps())], label=L"h = \sqrt{\epsilon}", color=:red, linestyle=:dash)
-savefig(p1, joinpath(plot_folder, "error_sin.png"))
+savefig(p1, joinpath(plot_folder, "error_sin.pdf"))
 
 # one can show, that the error is not monotonely decreasing for h->0. That ist because of rounding errrors.
 # From a error analysis point ov view, one gets O(eps()/h) as rounding errors and O(h) as an error of the method. eps() is here the machine precision
 # combining these two we obtain: g(h) = eps()/h + h. Minimizing g results in h = sqrt(eps()). This is actually a global minimum, since g''(h)<0
-#sqrt(eps()) ≈ 1.4901161193847656e-8. actually we can see this in the plot itself.
+# sqrt(eps()) ≈ 1.4901161193847656e-8. actually we can see this in the plot itself. For h < sqrt(eps()) the error starts to increase again. This is due to rounding errors in numerical differentiation.
 
 # Exercise 3
 function riemann_sum(f, a, b, n)
@@ -77,7 +77,7 @@ plot!(p2, n_values, [c1 * (10/n)^1 for n in n_values], label="O(1/n)",  linestyl
 plot!(p2, n_values, [c2 * (10/n)^2 for n in n_values], label="O(1/n²)", linestyle=:dash, linewidth = 5)
 plot!(p2, n_values, [c4 * (10/n)^4 for n in n_values], label="O(1/n⁴)", linestyle=:dash, linewidth = 4)
 
-savefig(p2, joinpath(plot_folder, "error_sin_integration.png"))
+savefig(p2, joinpath(plot_folder, "error_sin_integration.pdf"))
 
 # in this case we see, that the errors of the riemann sum and the trapezeudal rule allign with each other. 
 # normally I would not expect this. However, in this special periodic case these both methods seem to be identically due to the boundary terms sin(0)=sin(pi)=0
@@ -99,4 +99,4 @@ plot!(p3, n_values, [c1 * (10/n)^1 for n in n_values], label="O(1/n)",  linestyl
 plot!(p3, n_values, [c2 * (10/n)^2 for n in n_values], label="O(1/n²)", linestyle=:dash, linewidth = 5)
 plot!(p3, n_values, [c4 * (10/n)^4 for n in n_values], label="O(1/n⁴)", linestyle=:dash, linewidth = 4)
 
-savefig(p3, joinpath(plot_folder, "error_exp_integration.png"))
+savefig(p3, joinpath(plot_folder, "error_exp_integration.pdf"))
